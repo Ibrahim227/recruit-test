a8667306446b18173b5a065efc6548454da4ab93
mike <mamansani.ibrahim01@gmail.com>
2024-06-18 11:30:27 +0100

    update script/server.js

---
+++ script/server.js
@@ -0,0 +1,27 @@
+async function getToken() {
+    const username = 'coalition';
+    const password = 'skills-test';
+
+    try {
+        const response = await fetch("https://fedskillstest.coalitiontechnologies.workers.dev/", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username = username,
+                password = password
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data.token;
+    } catch (error) {
+        console.log("Error fetching token:", error);
+        throw error;
+    }
+}
