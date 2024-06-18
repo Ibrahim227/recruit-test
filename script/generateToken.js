5b89e5ed8b54eba5788ad59123e118ad8d746396
mike <mamansani.ibrahim01@gmail.com>
2024-06-18 17:31:14 +0100

    update script/server.js to script/generateToken.js

---
+++ script/generateToken.js
@@ -0,0 +1,32 @@
+---Trying to generate a Token. Apparently the endpoint API does not support "POST" requests. So, I done it with the "Basic-encoded-base64"---
+
+//async function getToken() {
+//    const username = 'coalition';
+//    const password = 'skills-test';
+//
+//    try {
+//        const response = await fetch("https://fedskillstest.coalitiontechnologies.workers.dev/", {
+//            method: 'POST',
+//            headers: {
+//                'Content-Type': 'application/json'
+//            },
+//            body: JSON.stringify({
+//                username = username,
+//                password = password
+//            })
+//        });
+//
+//        if (!response.ok) {
+//            throw new Error(`HTTP error! status ${response.status}`);
+//        }
+//
+//        const data = await response.json();
+//        return data.token;
+//    } catch (error) {
+//        console.log("Error fetching token:", error);
+//        throw error;
+//    }
+//}
+//
+//// export function for use
+//export { getToken };
